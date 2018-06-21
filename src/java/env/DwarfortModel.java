4534e6496e3d839afefb2ec5ccaced00fdbdd524
Luxor001 <stefano.belli.dev@gmail.com>
2018-06-21 15:01:56 +0200

    Initial commit

---
+++ src/java/env/DwarfortModel.java
@@ -0,0 +1,11 @@
+package env;
+
+import jason.environment.grid.GridWorldModel;
+
+public class DwarfortModel extends GridWorldModel{
+
+	protected DwarfortModel(int arg0, int arg1, int arg2) {
+		super(arg0, arg1, arg2);
+		// TODO Auto-generated constructor stub
+	} 
+}
