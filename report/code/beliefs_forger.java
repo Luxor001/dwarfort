fe618d246f52fb229ffedd22a417dfddd860f2fd
Luxor002 <stefano.belli4@studio.unibo.it>
2018-08-04 22:36:19 +0200

    piccole aggiunte sul report

--- report/code/beliefs_forger.java
+++ report/code/beliefs_forger.java
@@ -1,5 +1,9 @@
-thirstiness(0). // grado di sete dell'agente
-thirsty :- thirstiness(N) & N >= 10.
-strength_kg(10). // forza personale del minatore
-carrying(Resource, 0). // kg di materiale in trasporto
-atCapacity :- carrying(_, Kg) & strength_kg(S) & S <= Kg.
+needs(gold).
+
+items(armor, 0).
+
+//storageKg(Resource, Kg).
+
+//orkOrdeIncoming.
+
+//carrier(Name).
