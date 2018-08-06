74a2986f12e0a97f2222be73939c3b56e81ba152
Luxor001 <stefano.belli.dev@gmail.com>
2018-08-07 00:13:31 +0200

    Ultime fix per il report

--- report/code/beliefs_miner.java
+++ report/code/beliefs_miner.java
@@ -5,6 +5,9 @@ strength_kg(10).
 carrying(Resource, 0).
 atCapacity :- carrying(_, Kg) & strength_kg(S) & S <= Kg.
 
+direction(left).
+my_position(X,Y).
+
 //forgerNeeds(Resource)
 
 //resource(Resource, Location)
